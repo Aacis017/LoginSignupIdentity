83a9c16257afa09002ee36c59471606ba9b39477
Aacis017 <dhakalaashish17@gmail.com>
2023-06-22 14:27:26 +0545

    identityLoginSignup

---
+++ ManuallyLoginSIgnuP/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManuallyLoginSIgnuP.ViewModel
+{
+    public class RegisterViewModel
+    {
+        
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage ="Password and ConfirmPassword doesn't match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
